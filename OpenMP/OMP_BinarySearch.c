6b08fbe20ddd1d8e7a38fb1ff316082f2af8c776
Luis Andres Gonzalez Corzo <luisangoco26@gmail.com>
2024-03-20 19:45:39 -0500

    add the c files of the exercice

---
+++ OpenMP/OMP_BinarySearch.c
@@ -0,0 +1,87 @@
+/*
+ * C Program to Perform Binary Search using Recursion
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+//Import the OpenMP Library
+#include <omp.h>
+ 
+void binary_search(int [], int, int, int);
+void bubble_sort(int [], int);
+int randNumber(int, int);
+ 
+int main()
+{
+    int key, size, i;
+    int list[25];
+ 
+    printf("Enter size of a list: ");
+    scanf("%d", &size);
+    printf("Enter elements\n");
+    // Add the parallel sentence
+    #pragma omp parallel shared (list, size) private(i)
+    {
+        /*
+        parallel the input for, doing this the system
+        hasn't had to wait the user to type all inputs 
+        of the list
+        */ 
+        #pragma omp parallel for
+        for(i=0; i < size; i++){
+            list[i] = randNumber(-100,100);
+        }
+    }
+    bubble_sort(list, size);
+    printf("\n");
+    printf("Enter key to search\n");
+    scanf("%d", &key);
+    binary_search(list, 0, size, key); 
+}
+ 
+void bubble_sort(int list[], int size)
+{
+    int temp, i, j;
+    #pragma omp parallel shared (list, size, temp) private(i, j)
+    {
+        // parallel the sort Funtion
+        #pragma omp parallel for
+        for(i = 0; i < size; i++){
+            for(j = i; j < size; j++){
+                if (list[i] > list[j]){
+                    temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+    }
+}
+ 
+void binary_search(int list[], int lo, int hi, int key)
+{
+    int mid;
+ 
+    if (lo > hi)
+    {
+        printf("Key not found\n");
+        return;
+    }
+    mid = (lo + hi) / 2;
+    if (list[mid] == key)
+    {
+        printf("Key found\n");
+    }
+    else if (list[mid] > key)
+    {
+        binary_search(list, lo, mid - 1, key);
+    }
+    else if (list[mid] < key)
+    {
+        binary_search(list, mid + 1, hi, key);
+    }
+}
+//Function to create a random number to fill the array
+int randNumber (int min, int max){
+    return (int)(((double)rand()/RAND_MAX)*(max-min)+min);
+}
